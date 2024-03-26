280db04a3eb0173985aa608716f1e265f0c75e2c
Andri Hendrawan <andrihendrawan0606@gmail.com>
2024-03-26 20:10:47 +0700

    Pertemuan-2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,29 @@
+<?php
+class Contoh1 extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller iFF
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+		echo "<h1>Perkenalkan</h1>";
+ 		echo"Nama saya Imam Nawawi
+		Saya tingga di daerah Ciputat
+ 		olah raga yang saya sukai adalah
+ 		Bulutangkis";
+// 
+	}
+}
